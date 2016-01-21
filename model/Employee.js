9e1a8bf0f96ce3057a49032df33947f68e112e32
Jessica Tran Hur <jesshur824@gmail.com>
2016-01-21 10:42:34 -0800

    switching from unique email to unique user name in Schema

--- model/Employee.js
+++ model/Employee.js
@@ -2,10 +2,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Employee = new Schema({
-  name: {type: String, required: true},
-  username: {type: String, require: true},
-  DOB: {type: Date, required: true},
-  address: {type: String, require: true},
+  name: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    require: true,
+    unique: true,
+    index: true
+  },
+  DOB: {
+    type: Date,
+    required: true
+  },
+  address: {
+    type: String,
+    require: true
+  },
   phone: {
     type: String,
     require: true,
@@ -19,15 +33,17 @@ const Employee = new Schema({
   email: {
     type: String,
     require: true,
-    validate: {
-      validator: function(email) {
-        return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-      },
-      message: '{VALUE} is not a valid email!'
-    }
+    match: /.+\@.+\..+/
   },
-  position: {type: String, require: true},
-  manager: {type: Schema.Types.ObjectId, ref: 'Employee'}
+  position: {
+    type: String,
+    require: true,
+    enum: ['manager', 'accountant', 'engineer', 'receptionist']
+  },
+  manager: {
+    type: Schema.Types.ObjectId,
+    ref: 'Employee'
+  }
 });
 
 module.exports = mongoose.model('Employee', Employee);
