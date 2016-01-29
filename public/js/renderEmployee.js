dc348aadbf2f466c088f1bdda0b2e426ece55f01
Jessica Tran Hur <jesshur824@gmail.com>
2016-01-28 16:11:52 -0800

    working mechanism for delete employee

--- public/js/renderEmployee.js
+++ public/js/renderEmployee.js
@@ -5,4 +5,25 @@ employeeApp.controller('EmployeeController', function($scope, $http) {
   $http.get('/employees').then( function( res ) {
     $scope.employees = res.data;//Angular specific
   });
+  $scope.delete = function(employee) {
+    $scope.deleteEmployeeId = employee._id;
+    $http.delete('/employees/'+$scope.deleteEmployeeId)
+         .then(
+          function(res){
+            console.log(res.data);
+            $scope.deleteConfirmation = 'Deleted Employee:';
+            $scope.deletedEmployee = res.data;
+            var temp = [];
+            $scope.employees.forEach(function(employee){
+              if (employee._id != $scope.deleteEmployeeId){
+                temp.push(employee);
+              }
+            });
+            $scope.employees = temp;
+          },
+          function(err){
+            $scope.deleteConfirmation = res.statusText;
+          });
+  }
 });
+
