2a35b51109bb536115850c4f0a36db42bb28f2cb
Jessica Tran Hur <jesshur824@gmail.com>
2016-01-29 13:31:13 -0800

    change to employee.index for easier tracking

--- public/js/renderEmployee.js
+++ public/js/renderEmployee.js
@@ -7,25 +7,27 @@ employeeApp.controller('EmployeeController', function($scope, $http) {
     $scope.employees = res.data;//Angular specific(?)
     $scope.employees.forEach(function(employee){
       employee.DOB = employee.DOB.substring(0,10);
+      employee.index = employee._id;
     });
   });
 
   //DELETE
   $scope.delete = function(employee) {
-    $scope.deleteEmployeeId = employee._id;
-    $http.delete('/employees/'+$scope.deleteEmployeeId)
+    $scope.deleteEmployeeIndex = employee.index;
+    $http.delete('/employees/'+employee._id)
          .then(
           function(res){
-            $scope.deleteConfirmation = 'Deleted Employee:';
-            $scope.deletedEmployee = res.data;
-            $scope.deletedEmployee.DOB = $scope.deletedEmployee.DOB.substring(0,10);
             var temp = [];
             $scope.employees.forEach(function(employee){
-              if (employee._id != $scope.deleteEmployeeId){
+              if (employee.index != $scope.deleteEmployeeIndex){
                 temp.push(employee);
               }
             });
             $scope.employees = temp;
+
+            $scope.deleteConfirmation = 'Deleted Employee:';
+            $scope.deletedEmployee = res.data;
+            $scope.deletedEmployee.DOB = $scope.deletedEmployee.DOB.substring(0,10);
           },
           function(err){
             $scope.deleteConfirmation = res.statusText;
@@ -35,21 +37,23 @@ employeeApp.controller('EmployeeController', function($scope, $http) {
   //EDIT-PUT/PATCH
   $scope.edit = function(employee) {
     $scope.newEmployee = employee;
-    $scope.idHolder = $scope.newEmployee._id;
+    // $scope.idHolder = $scope.newEmployee._id;
     $scope.editEmployee = true;
   }
   $scope.cancelEdit = function() {
     $scope.badRequest = false;
-    $http.get('/employees/'+$scope.idHolder)
+    $http.get('/employees/'+$scope.newEmployee.index)
          .then(
             function(res) {
               var temp = [];
               $scope.employees.forEach(function(employee){
-                if (employee._id != $scope.newEmployee._id){
+                if (employee.index != $scope.newEmployee.index){
                   temp.push(employee);
                 }
               });
               $scope.employees = temp;
+              res.data.index = res.data._id;
+              res.data.DOB = res.data.DOB.substring(0,10);
               $scope.employees.push(res.data);
               $scope.newEmployee = null;
             },
